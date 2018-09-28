9cbfa695ac76ba33a989760d213ea4454f11c93f
Yilong Zhu <me@yilongzhu.com>
2018-09-28 16:38:38 -0400

    added user blurbs and base template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Yilong'}
+    posts = [
+            {'author': {'username': 'Daniel'},
+             'body': 'Wow, I can\'t believe I skipped gymnastics to dance.'},
+            {'author': {'username': 'Sam'},
+             'body': 'It must be weird to go from taking a Lyft to Target to going to class.'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
