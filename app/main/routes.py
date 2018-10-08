b3745ceb6db4af42a271d5065fb304f685b2578a
Yilong Zhu <me@yilongzhu.com>
2018-10-07 21:17:47 -0400

    restructured file structure

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, flash, redirect, url_for, request
-from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
-from flask_login import current_user, login_user, logout_user, login_required
+from flask import current_app, render_template, flash, redirect, url_for, request
+from app import db
+from app.main.forms import EditProfileForm, PostForm
+from app.main import bp
+from flask_login import current_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
@@ -18,90 +19,46 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash('Post saved!')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
 
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
+        page, current_app.config['POSTS_PER_PAGE'], False)
 
-    next_url = url_for('index', page=posts.next_num) \
+    next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('main.index', page=posts.prev_num) \
         if posts.has_prev else None
 
     return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-
-    return render_template('login.html', title='Login', form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('You are now registered.')
-        return redirect(url_for('login'))
-    
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/u/<username>')
+@bp.route('/u/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False
+        page, current_app.config['POSTS_PER_PAGE'], False
     )
 
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
+    next_url = url_for('main.user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
 
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
-@app.before_request
+@bp.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -111,7 +68,7 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('auth.edit_profile'))
     elif request.method == 'GET':
         form.avatar.data = current_user.avatar
         form.username.data = current_user.username
@@ -120,41 +77,41 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
-@app.route('/follow/<username>')
+@bp.route('/follow/<username>')
 @login_required
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash('You cannot follow yourself!')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash('You\'re now following {}!'.format(username))
 
-    return redirect(url_for('user', username=username))
+    return redirect(url_for('main.user', username=username))
 
 
-@app.route('/unfollow/<username>')
+@bp.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash('You cannot unfollow yourself!')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
     flash('You no long follow {}!'.format(username))
 
-    return redirect(url_for('user', username=username))
+    return redirect(url_for('main.user', username=username))
 
 
-@app.route('/u')
+@bp.route('/u')
 @login_required
 def all_users():
     users = User.query.order_by(User.username.asc()).all()
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
